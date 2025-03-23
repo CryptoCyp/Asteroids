e49e46dccf82354f6a474a677086fe60639c3895
CryptoCyp <etolay7@gmail.com>
2025-03-23 23:31:20 +0100

    add game loop

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-# this allows us to use code from
+# This allows us to use code from
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    
+    
+    
+    # Set up the display
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        
+        # Clear screen with black
+        screen.fill("black")
+        
+        # Update display
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
